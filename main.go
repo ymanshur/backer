979eaa716c0cc5ed437af6fc86fec7da50eee4d5
Yusuf Manshur <ymanshur@gmail.com>
2022-10-17 16:38:49 +0700

    feat(main): display data from db

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"backer/user"
 	"fmt"
 	"log"
 
@@ -11,10 +12,23 @@ import (
 func main() {
 	// refer https://github. .com/go-sal-driver/mysql#dsn-data-source-name for details
 	dsn := "root:@tcp(127.0.0.1:3306)/backer?charset=utf8mb4&parseTime-True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection to database is good")
+
+	var users []user.User
+	fmt.Println(len(users))
+
+	db.Find(&users)
+
+	fmt.Println(len(users))
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("===============")
+	}
 }
