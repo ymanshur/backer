066062aa277c1f189fd22e3ced5f5d9db7175c4d
Yusuf Manshur <ymanshur@gmail.com>
2022-10-17 14:02:03 +0700

    feat: add gorm mysql connection

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// refer https://github. .com/go-sal-driver/mysql#dsn-data-source-name for details
+	dsn := "root:@tcp(127.0.0.1:3306)/backer?charset=utf8mb4&parseTime-True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Connection to database is good")
+}
