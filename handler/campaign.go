92d903824b4128e1b81f901964d5f93d0c225a95
Yusuf Manshur <ymanshur@gmail.com>
2022-10-18 16:17:18 +0700

    feat(campaign): add GetCampaigns handler

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,41 @@
 package handler
+
+import (
+	"backer/campaign"
+	"backer/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+func (h *campaignHandler) GetCampaigns(ctx *gin.Context) {
+	userID, err := strconv.Atoi(ctx.Query("user_id"))
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", errorMessage)
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login failed", http.StatusInternalServerError, "error", errorMessage)
+		ctx.JSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaigns)
+	ctx.JSON(http.StatusOK, response)
+}
