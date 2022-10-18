412380bf3b41f5c817db8a57305a75ec12766411
Yusuf Manshur <ymanshur@gmail.com>
2022-10-18 10:19:17 +0700

    feat(main): try login service

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"backer/handler"
 	"backer/user"
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -12,14 +13,38 @@ import (
 
 func main() {
 	// refer https://github. .com/go-sal-driver/mysql#dsn-data-source-name for details
-	dsn := "root:@tcp(127.0.0.1:3306)/backer?charset=utf8mb4&parseTime-True&loc=Local"
+	dsn := "root:@tcp(127.0.0.1:3306)/backer?charset=utf8mb4&parseTime=true&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	userRepository := user.NewRepository(db)
+
+	// userByEmail, err := userRepository.FindByEmail("ymanshur@gmail.com")
+	// if err != nil {
+	// 	log.Fatalln(err.Error())
+	// }
+	// if userByEmail.ID == 0 {
+	// 	log.Fatalln("User tidak ditemukan")
+	// } else {
+	// 	fmt.Println(userByEmail.Name)
+	// }
+
 	userService := user.NewService(userRepository)
+
+	input := user.LoginInput{
+		Email:    "ymanshur@gmail.com",
+		Password: "password",
+	}
+	user, err := userService.Login(input)
+	if err != nil {
+		fmt.Println("Terjadi kesalahan")
+		log.Fatalln(err.Error())
+	}
+	fmt.Println(user.Email)
+	fmt.Println(user.Name)
+
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
