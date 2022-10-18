d357482491216cc261b53b4938057f12695d77eb
Yusuf Manshur <ymanshur@gmail.com>
2022-10-18 12:01:07 +0700

    feat(service): add SaveAvatar method

--- user/service.go
+++ user/service.go
@@ -10,6 +10,7 @@ type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
 	Login(input LoginInput) (User, error)
 	IsEmailAvailable(input CheckEmailInput) (bool, error)
+	SaveAvatar(id int, fileLocation string) (User, error)
 }
 
 type service struct {
@@ -75,4 +76,26 @@ func (s *service) IsEmailAvailable(input CheckEmailInput) (bool, error) {
 	}
 
 	return false, nil
+}
+
+func (s *service) SaveAvatar(id int, fileLocation string) (User, error) {
+	/**
+	 * 1. Get user by id
+	 * 2. Update avatar file name
+	 * 3. Save updated avatar file name
+	 */
+
+	user, err := s.repository.FindByID(id)
+	if err != nil {
+		return user, err
+	}
+
+	user.AvatarFileName = fileLocation
+
+	updatedUser, err := s.repository.Update(user)
+	if err != nil {
+		return updatedUser, err
+	}
+
+	return updatedUser, nil
 }
