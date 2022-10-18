72c95155a3a37716e28ce3366e2b6a831061809f
Yusuf Manshur <ymanshur@gmail.com>
2022-10-18 11:53:14 +0700

    feat(repository): add FindByID and Update method

--- user/repository.go
+++ user/repository.go
@@ -5,6 +5,8 @@ import "gorm.io/gorm"
 type Repository interface {
 	Save(user User) (User, error)
 	FindByEmail(email string) (User, error)
+	FindByID(id int) (User, error)
+	Update(user User) (User, error)
 }
 
 type repository struct {
@@ -29,6 +31,25 @@ func (r *repository) FindByEmail(email string) (User, error) {
 
 	err := r.db.Where("email = ?", email).Find(&user).Error
 	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *repository) FindByID(id int) (User, error) {
+	var user User
+
+	err := r.db.Where("id = ?", id).Find(&user).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *repository) Update(user User) (User, error) {
+	if err := r.db.Save(&user).Error; err != nil {
 		return user, err
 	}
 
