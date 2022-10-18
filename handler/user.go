f59d556e001364b36abb84187e990ca17d0b40fc
Yusuf Manshur <ymanshur@gmail.com>
2022-10-18 07:23:51 +0700

    feat(RegisterUser): add register user handling

--- handler/user.go
+++ handler/user.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"backer/user"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,9 +17,23 @@ func NewUserHandler(userService user.Service) *userHandler {
 
 func (h *userHandler) RegisterUser(ctx *gin.Context) {
 	/**
-	* RegisterUser handler:
-	* 1. Get client input
-	* 2. Map the input into RegisterUserInput struct (DTO)
-	* 3. Pass the DTO into service
+	 * RegisterUser handler:
+	 * 1. Get client input
+	 * 2. Map the input into RegisterUserInput struct (DTO)
+	 * 3. Pass the DTO into service
 	 */
+
+	var input user.RegisterUserInput
+
+	err := ctx.ShouldBindJSON(&input)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, nil)
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, nil)
+	}
+
+	ctx.JSON(http.StatusOK, newUser)
 }
