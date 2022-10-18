16703b0763b54b6ec586d2d5495fabeb17f817d0
Yusuf Manshur <ymanshur@gmail.com>
2022-10-18 16:46:26 +0700

    feat(formatter): add campaign formatter

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,43 @@
 package campaign
+
+type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	UserID           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageURL         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	formatter := CampaignFormatter{
+		ID:               campaign.ID,
+		UserID:           campaign.UserID,
+		Name:             campaign.Name,
+		ImageURL:         "",
+		ShortDescription: campaign.ShortDescription,
+		GoalAmount:       campaign.GoalAmount,
+		CurrentAmount:    campaign.CurrentAmount,
+	}
+
+	if len(campaign.CampaignImages) > 0 {
+		formatter.ImageURL = campaign.CampaignImages[0].FileName
+	}
+
+	return formatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	if len(campaigns) == 0 {
+		return []CampaignFormatter{}
+	}
+
+	var formatters []CampaignFormatter
+
+	for _, campaign := range campaigns {
+		formatters = append(formatters, FormatCampaign(campaign))
+	}
+
+	return formatters
+}
