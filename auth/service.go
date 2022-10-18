75e3aa42a1fbb97356cb98594d4d51a4be1c52d0
Yusuf Manshur <ymanshur@gmail.com>
2022-10-18 12:45:51 +0700

    feat(auth): add GenerateToken service

---
+++ auth/service.go
@@ -0,0 +1,27 @@
+package auth
+
+import "github.com/dgrijalva/jwt-go"
+
+type Service interface {
+	GenerateToken(userID int) (string, error)
+}
+
+type service struct {
+}
+
+var SECRET_KEY = []byte("BWABACKERSTARTUP_53cr3t_k3y")
+
+func (s *service) GenerateToken(userID int) (string, error) {
+	claim := jwt.MapClaims{
+		"user_id": userID,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+
+	signedToken, err := token.SignedString(SECRET_KEY)
+	if err != nil {
+		return signedToken, err
+	}
+
+	return signedToken, nil
+}
