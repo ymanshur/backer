66fc6ea1cc05e893876fac9cd298038077446eeb
Yusuf Manshur <ymanshur@gmail.com>
2022-10-18 07:48:46 +0700

    fix(helper): make struct and their attribute public

--- helper/helper.go
+++ helper/helper.go
@@ -1,26 +1,26 @@
 package helper
 
-type response struct {
-	meta meta
-	data interface{}
+type Response struct {
+	Meta Meta        `json:"meta"`
+	Data interface{} `json:"data"`
 }
 
-type meta struct {
-	message string
-	code    int
-	status  string
+type Meta struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+	Status  string `json:"status"`
 }
 
-func APIResponse(message string, code int, status string, data interface{}) response {
-	meta := meta{
-		message: message,
-		code:    code,
-		status:  status,
+func APIResponse(message string, code int, status string, data interface{}) Response {
+	meta := Meta{
+		Message: message,
+		Code:    code,
+		Status:  status,
 	}
 
-	jsonResponse := response{
-		meta: meta,
-		data: data,
+	jsonResponse := Response{
+		Meta: meta,
+		Data: data,
 	}
 
 	return jsonResponse
