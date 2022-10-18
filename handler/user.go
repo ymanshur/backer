fd4c71c7f0d3862dc73e26c817b0551dd30eb5e5
Yusuf Manshur <ymanshur@gmail.com>
2022-10-18 08:15:35 +0700

    feat(main): using helper to formatting validation error

--- handler/user.go
+++ handler/user.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 )
 
 type userHandler struct {
@@ -29,12 +28,7 @@ func (h *userHandler) RegisterUser(ctx *gin.Context) {
 
 	err := ctx.ShouldBindJSON(&input)
 	if err != nil {
-		var errors []string
-
-		for _, e := range err.(validator.ValidationErrors) {
-			errors = append(errors, e.Error())
-		}
-
+		errors := helper.FormatValidationError(err)
 		errorMessage := gin.H{"errors": errors}
 
 		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
