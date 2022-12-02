3eacbcb1bde46a4d9ae07d460d3265376ad1dff7
Yusuf Manshur <ymanshur@gmail.com>
2022-12-02 11:06:23 +0700

    feat(campaign/service): add GetCampaignByID method

--- campaign/service.go
+++ campaign/service.go
@@ -2,6 +2,7 @@ package campaign
 
 type Service interface {
 	GetCampaigns(userID int) ([]Campaign, error)
+	GetCampaignByID(input GetCampaignInput) (Campaign, error)
 }
 
 type service struct {
@@ -28,4 +29,13 @@ func (s *service) GetCampaigns(userID int) ([]Campaign, error) {
 	}
 
 	return campaigns, err
+}
+
+func (s *service) GetCampaignByID(input GetCampaignInput) (Campaign, error) {
+	campaign, err := s.repository.FindByID(input.ID)
+	if err != nil {
+		return campaign, err
+	}
+
+	return campaign, err
 }
