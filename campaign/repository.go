10210ed138686c284972e1c6d12efb1fe38994a6
Yusuf Manshur <ymanshur@gmail.com>
2022-12-02 13:11:34 +0700

    feat(campign/repository): add Save method

--- campaign/repository.go
+++ campaign/repository.go
@@ -6,6 +6,7 @@ type Repository interface {
 	FindAll() ([]Campaign, error)
 	FindByUserID(userID int) ([]Campaign, error)
 	FindByID(ID int) (Campaign, error)
+	Save(campaign Campaign) (Campaign, error)
 }
 
 type repository struct {
@@ -47,6 +48,14 @@ func (r *repository) FindByID(ID int) (Campaign, error) {
 		Preload("User").
 		Preload("CampaignImages").
 		Find(&campaign).Error; err != nil {
+		return campaign, err
+	}
+
+	return campaign, nil
+}
+
+func (r *repository) Save(campaign Campaign) (Campaign, error) {
+	if err := r.db.Create(&campaign).Error; err != nil {
 		return campaign, err
 	}
 
