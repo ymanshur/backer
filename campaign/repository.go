d0caac6d800d7d10267d71566d1f9031756ad8a9
Yusuf Manshur <ymanshur@gmail.com>
2022-12-02 15:42:58 +0700

    feat(campaign/repository): add CreateImage method

--- campaign/repository.go
+++ campaign/repository.go
@@ -8,6 +8,7 @@ type Repository interface {
 	FindByID(ID int) (Campaign, error)
 	Save(campaign Campaign) (Campaign, error)
 	Update(campaign Campaign) (Campaign, error)
+	CreateImage(campaignImage CampaignImage) (CampaignImage, error)
 }
 
 type repository struct {
@@ -69,4 +70,12 @@ func (r *repository) Update(campaign Campaign) (Campaign, error) {
 	}
 
 	return campaign, nil
+}
+
+func (r *repository) CreateImage(campaignImage CampaignImage) (CampaignImage, error) {
+	if err := r.db.Create(&campaignImage).Error; err != nil {
+		return campaignImage, err
+	}
+
+	return campaignImage, nil
 }
